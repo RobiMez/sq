2299e387eb6d8059fb81c84c13e7023b297e469c
Robi <robelmezemir@gmail.com>
2024-03-31 17:29:10 +0300

    pause site

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,6 +9,7 @@
 	let uuid: string;
 	let loadingQuests = false;
 	let hideOriginals = false;
+	let paused = true;
 
 	const apiCall = async () => {
 		loadingQuests = true;
@@ -104,87 +105,100 @@
 	// TODO: Need a staging system where a user can accept or modify the quest
 </script>
 
-<div
-	class="container mx-auto flex h-full w-full max-w-4xl flex-grow flex-col items-center justify-start px-12"
->
-	<div class="flex w-full flex-col gap-1 bg-stone-100 px-4">
-		<h1 class="py-6 text-3xl font-extralight text-stone-600 lg:text-5xl">Welcome to Side Quests</h1>
-		<div class="flex flex-row items-center justify-start gap-2 py-1">
-			<input
-				type="checkbox"
-				class="toggle toggle-xs"
-				bind:checked={justMine}
-				on:click={handleMineChange}
-			/>
-			<small class="whitespace-nowrap"> Just mine </small>
-			<input type="checkbox" class="toggle toggle-xs" bind:checked={hideCompleted} />
-			<small class="whitespace-nowrap"> Hide done </small>
-			<input type="checkbox" class="toggle toggle-xs" bind:checked={hideOriginals} />
-			<small class="whitespace-nowrap"> Hide originals </small>
-			{#if loadingQuests}
-				<small> Refreshing ... </small>
-			{/if}
-		</div>
-		<div class="flex w-full flex-row items-center justify-center gap-4 py-2 pb-12">
-			<input
-				bind:value={vagueQuest}
-				type="text"
-				class="input input-sm input-bordered w-full rounded-none"
-				placeholder="I want to ... "
-				on:keydown={(e) => e.key === 'Enter' && vagueQuest.trim().length > 0 && generateQuest()}
-			/>
-			<button
-				class="btn btn-sm rounded-none border border-black disabled:border-stone-700"
-				disabled={vagueQuest.trim().length <= 0 || generatingQuest}
-				on:click={generateQuest}
-			>
-				{#if generatingQuest}
-					Generating Quest 🧙‍♂️
-				{:else}
-					Generate Quest ✨
+{#if !paused}
+	<div
+		class="container mx-auto flex h-full w-full max-w-4xl flex-grow flex-col items-center justify-start px-12"
+	>
+		<div class="flex w-full flex-col gap-1 bg-stone-100 px-4">
+			<h1 class="py-6 text-3xl font-extralight text-stone-600 lg:text-5xl">
+				Welcome to Side Quests
+			</h1>
+			<div class="flex flex-row items-center justify-start gap-2 py-1">
+				<input
+					type="checkbox"
+					class="toggle toggle-xs"
+					bind:checked={justMine}
+					on:click={handleMineChange}
+				/>
+				<small class="whitespace-nowrap"> Just mine </small>
+				<input type="checkbox" class="toggle toggle-xs" bind:checked={hideCompleted} />
+				<small class="whitespace-nowrap"> Hide done </small>
+				<input type="checkbox" class="toggle toggle-xs" bind:checked={hideOriginals} />
+				<small class="whitespace-nowrap"> Hide originals </small>
+				{#if loadingQuests}
+					<small> Refreshing ... </small>
 				{/if}
-			</button>
+			</div>
+			<div class="flex w-full flex-row items-center justify-center gap-4 py-2 pb-12">
+				<input
+					bind:value={vagueQuest}
+					type="text"
+					class="input input-sm input-bordered w-full rounded-none"
+					placeholder="I want to ... "
+					on:keydown={(e) => e.key === 'Enter' && vagueQuest.trim().length > 0 && generateQuest()}
+				/>
+				<button
+					class="btn btn-sm rounded-none border border-black disabled:border-stone-700"
+					disabled={vagueQuest.trim().length <= 0 || generatingQuest}
+					on:click={generateQuest}
+				>
+					{#if generatingQuest}
+						Generating Quest 🧙‍♂️
+					{:else}
+						Generate Quest ✨
+					{/if}
+				</button>
+			</div>
+		</div>
+
+		<div class="flex w-full flex-col gap-8 py-12">
+			{#key quests}
+				{#each quests as quest, i}
+					{#key hideCompleted}
+						{#if hideCompleted}
+							{#if quest.progress !== 100}
+								<Card
+									on:refreshPlz={async () => {
+										await apiCall();
+									}}
+									{quest}
+									{quests}
+									{i}
+								/>
+							{/if}
+						{:else if hideOriginals}
+							{#if !quest.cloners.find((c) => c === localStorage.getItem('uuid'))}
+								<Card
+									on:refreshPlz={async () => {
+										await apiCall();
+									}}
+									{quest}
+									{quests}
+									{i}
+								/>
+							{/if}
+						{:else}
+							<Card
+								on:refreshPlz={async () => {
+									await apiCall();
+								}}
+								{quest}
+								{quests}
+								{i}
+							/>
+						{/if}
+					{/key}
+				{/each}
+			{/key}
 		</div>
 	</div>
-
-	<div class="flex w-full flex-col gap-8 py-12">
-		{#key quests}
-			{#each quests as quest, i}
-				{#key hideCompleted}
-					{#if hideCompleted}
-						{#if quest.progress !== 100}
-							<Card
-								on:refreshPlz={async () => {
-									await apiCall();
-								}}
-								{quest}
-								{quests}
-								{i}
-							/>
-						{/if}
-					{:else if hideOriginals}
-						{#if !quest.cloners.find((c) => c === localStorage.getItem('uuid'))}
-							<Card
-								on:refreshPlz={async () => {
-									await apiCall();
-								}}
-								{quest}
-								{quests}
-								{i}
-							/>
-						{/if}
-					{:else}
-						<Card
-							on:refreshPlz={async () => {
-								await apiCall();
-							}}
-							{quest}
-							{quests}
-							{i}
-						/>
-					{/if}
-				{/key}
-			{/each}
-		{/key}
+{:else}
+	<div class="flex h-screen w-screen flex-col items-center justify-center gap-12">
+		<h1 class="flex flex-row items-center justify-center text-6xl font-black text-stone-600">
+			Site paused 😓
+		</h1>
+		<small class="flex flex-row items-center justify-center text-md font-medium text-stone-800">
+			A lot of hate & troll content was posted , Gimme a moment to clean that up.
+		</small>
 	</div>
-</div>
+{/if}
